94596a86e9844cb0d5418e51e9e4ca59cf77e7c2
KULSUM <lit2021021@iiitl.ac.in>
2022-10-02 01:27:49 +0530

    141. Linked List Cycle: cpp solution

---
+++ CPP/Linked list cycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while( fast != NULL && fast->next != NULL ){
+            fast = fast->next->next;
+            slow = slow->next;
+            if( fast == slow ){
+                return true;
+            }
+        }
+        return false;
+    
+    }
+};
