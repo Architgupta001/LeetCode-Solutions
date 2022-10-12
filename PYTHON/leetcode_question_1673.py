93fb5039156f351e44ddf651088717fea60ba51e
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2022-10-12 20:02:39 +0900

    Added for problem 1673 Rich Customer Wealth

---
+++ PYTHON/leetcode_question_1673.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        rlt = []
+        for i in range(len(accounts)):
+            result = 0
+            for j in range(len(accounts[i])):
+                result += accounts[i][j]
+            rlt.append(result)
+        rlt.sort()
+        return rlt[len(rlt) - 1]
