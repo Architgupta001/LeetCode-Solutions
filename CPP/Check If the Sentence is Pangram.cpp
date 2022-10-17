4b88dc167468bf045e8475441e7268f310a92106
anuj mishra <anujmishraan2005@gmail.com>
2022-10-17 22:34:40 +0530

    Leetcode Daily Problem 17th October 2022 in C++

---
+++ CPP/Check If the Sentence is Pangram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int arr[26] = {0};
+        int index = 0;
+        for(int i=0;i<sentence.size();i++){
+            index=sentence[i] - 'a';
+            arr[index]++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+1
